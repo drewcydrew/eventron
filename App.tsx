5294dcd7600e3e475c9564eaba2a66dd7f7a80e6
Andrew <andrew/jovaras@apac.bmt.org>
2025-04-07 01:09:10 +1000

    Adding boilerplate code

--- App.tsx
+++ App.tsx
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import BlankScreen from "./src/screens/DefineScreen";
+import SecondScreen from "./src/screens/RunScreen";
+
+export type TabParamList = {
+  Define: undefined;
+  Run: undefined;
+};
+
+const Tab = createBottomTabNavigator<TabParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "#333333",
+          tabBarInactiveTintColor: "#666666",
+          tabBarStyle: {
+            borderTopColor: "rgba(0, 0, 0, 0.1)",
+            borderTopWidth: 0.5,
+          },
+        }}
+      >
+        <Tab.Screen
+          name="Define"
+          component={BlankScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="build-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Run"
+          component={SecondScreen}
+          options={({ navigation, route }) => ({
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="bar-chart-outline" size={size} color={color} />
+            ),
+          })}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
